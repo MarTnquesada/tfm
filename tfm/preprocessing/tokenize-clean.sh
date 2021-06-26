d40370e1a0a0eae32f7eca5694faee434f35fec4
Martín Quesada <martin@datamaran.com>
2021-06-26 17:30:07 +0200

    correctly indicated language when tokenizing and cleaning with moses

--- tfm/preprocessing/tokenize-clean.sh
+++ tfm/preprocessing/tokenize-clean.sh
@@ -5,7 +5,7 @@ lang1=$2
 lang2=$3
 
 # Check out http://www.statmt.org/wmt07/baseline.html
-docker container run -it --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/tokenizer/tokenizer.perl ${lang1} < ${file}.${lang1} > ${file}.tok.${lang1}"
-docker container run -it --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/tokenizer/tokenizer.perl ${lang2} < ${file}.${lang2} > ${file}.tok.${lang2}"
+docker container run -it --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/tokenizer/tokenizer.perl -l ${lang1} < ${file}.${lang1} > ${file}.tok.${lang1}"
+docker container run -it --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/tokenizer/tokenizer.perl -l ${lang2} < ${file}.${lang2} > ${file}.tok.${lang2}"
 
 docker container run -it --rm -v ${PWD}/data/:/data moses scripts/training/clean-corpus-n.perl ${file}.tok ${lang1} ${lang2} ${file}.clean 1 40
