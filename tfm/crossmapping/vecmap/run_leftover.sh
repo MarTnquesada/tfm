8918161d5e1879b9a17fe8ece31007e74ec4b611
Martín Quesada <martin@datamaran.com>
2021-08-21 23:10:32 +0200

    added evaluation to run_leftover in vecmap

--- tfm/crossmapping/vecmap/run_leftover.sh
+++ tfm/crossmapping/vecmap/run_leftover.sh
@@ -16,3 +16,68 @@ for lang in fr de ru hi; do
         python -m tfm.crossmapping.vecmap.map_embeddings data/embeddings/monolingual-${lang}-3-6-${dimension}.vec data/embeddings/monolingual-en-3-6-${dimension}.vec data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec --batch_size 1000 --unsupervised --verbose
     done
 done
+
+echo
+echo ------------ VECMAP ------------
+echo ------- \(BASE TOKENIZATION\) -------
+echo ---\[COSINE SIMILARITY\]---
+echo LANG   ---   DIMENSION
+lang=de
+dimension=100
+echo ${lang}   ---   ${dimension}
+python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+    data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+    data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+    -d data/datasets/eval_dicts/${lang}-en.txt \
+    --retrieval csls --cuda
+for lang in ru hi; do
+    for dimension in 50 100; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+            data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+            data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+            -d data/datasets/eval_dicts/${lang}-en.txt \
+            --retrieval csls --cuda
+    done
+done
+for lang in fr de ru hi; do
+    for dimension in 400 500; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+        data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls --cuda
+    done
+done
+
+echo ---\[L2 DISTANCE\]---
+echo LANG   ---   DIMENSION
+lang=de
+dimension=100
+echo ${lang}   ---   ${dimension}
+python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+    data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+    data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+    -d data/datasets/eval_dicts/${lang}-en.txt \
+    --retrieval csls --dot --cuda
+for lang in ru hi; do
+    for dimension in 50 100; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+            data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+            data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+            -d data/datasets/eval_dicts/${lang}-en.txt \
+            --retrieval csls --dot --cuda
+    done
+done
+for lang in fr de ru hi; do
+    for dimension in 400 500; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+        data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls --dot --cuda
+    done
+done
