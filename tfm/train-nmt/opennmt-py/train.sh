a268c06683185cc9497f79c53938095b7afcb2b4
Martín Quesada <martin@datamaran.com>
2021-08-26 19:32:16 +0200

    updated nmt train command

--- tfm/train-nmt/opennmt-py/train.sh
+++ tfm/train-nmt/opennmt-py/train.sh
@@ -9,9 +9,12 @@ do
 done
 
 # If using BPE, no need to generate vocab, instead use pre-generated BPE dictionary
-if [ ${bpe} = "" ]; then
+if [ -z ${bpe+x} ]
+then
     echo "Building vocab for model ${config}"
     onmt_build_vocab -config ${config} -n_sample 10000
+else
+    echo "BPE flag activated, assuming that vocabulary is already present"
 fi
 
 echo "Launching Transformer model training for ${config}"
