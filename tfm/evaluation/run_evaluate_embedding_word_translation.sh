b15f39b3c9564c4b59d913b320c5fd594c783301
Martín Quesada <martin@datamaran.com>
2021-08-26 17:42:14 +0200

    moved vecmap and baseline evaluation of embeddings to the bottom of the running script

--- tfm/evaluation/run_evaluate_embedding_word_translation.sh
+++ tfm/evaluation/run_evaluate_embedding_word_translation.sh
@@ -1,6 +1,63 @@
 #!/usr/bin/env bash
 
 echo
+echo ------------ MUSE ------------
+echo ------- \(BASE TOKENIZATION\) -------
+echo ---\[COSINE SIMILARITY\]---
+echo LANG   ---   DIMENSION
+for lang in fr de ru hi; do
+    for dimension in 50 100 150 200 250 300; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-${lang}.txt \
+        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-en.txt \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls
+    done
+done
+echo ---\[L2 DISTANCE\]---
+echo LANG   ---   DIMENSION
+for lang in fr de ru hi; do
+    for dimension in 50 100 150 200 250 300; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-${lang}.txt \
+        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-en.txt \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls --dot
+    done
+done
+
+
+echo
+echo ------------ CONCAT------------
+echo ------- \(BASE TOKENIZATION\) -------
+echo ---\[COSINE SIMILARITY\]---
+echo LANG   ---   DIMENSION
+for lang in fr de ru hi; do
+    for dimension in 50 100 150 200 250 300; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls
+    done
+done
+echo ---\[L2 DISTANCE\]---
+echo LANG   ---   DIMENSION
+for lang in fr de ru hi; do
+    for dimension in 50 100 150 200 250 300; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls --dot
+    done
+done
+
+echo
 echo ------------ BASELINE ------------
 echo ------- \(BASE TOKENIZATION\) -------
 echo ---\[COSINE SIMILARITY\]---
@@ -53,64 +110,6 @@ for lang in fr de ru hi; do
         python3 -m tfm.evaluation.evaluate_embedding_word_translation \
         data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
         data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls --dot
-    done
-done
-
-
-echo
-echo ------------ MUSE ------------
-echo ------- \(BASE TOKENIZATION\) -------
-echo ---\[COSINE SIMILARITY\]---
-echo LANG   ---   DIMENSION
-for lang in fr de ru hi; do
-    for dimension in 50 100 150 200 250 300; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-${lang}.txt \
-        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-en.txt \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls
-    done
-done
-echo ---\[L2 DISTANCE\]---
-echo LANG   ---   DIMENSION
-for lang in fr de ru hi; do
-    for dimension in 50 100 150 200 250 300; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-${lang}.txt \
-        data/embeddings/monolingual-${lang}-en-muse/${dimension}/vectors-en.txt \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls --dot
-    done
-done
-
-
-echo
-echo ------------ CONCAT------------
-echo ------- \(BASE TOKENIZATION\) -------
-echo ---\[COSINE SIMILARITY\]---
-echo LANG   ---   DIMENSION
-for lang in fr de ru hi; do
-    for dimension in 50 100 150 200 250 300; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls
-    done
-done
-echo ---\[L2 DISTANCE\]---
-echo LANG   ---   DIMENSION
-for lang in fr de ru hi; do
-    for dimension in 50 100 150 200 250 300; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
         -d data/datasets/eval_dicts/${lang}-en.txt \
         --retrieval csls --dot
     done
