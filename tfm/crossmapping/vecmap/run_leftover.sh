d5779851b31998d7e8dcb948cec5d6e1a9a1936a
Martín Quesada <martin@datamaran.com>
2021-08-28 22:17:52 +0200

    modified run_leftover to only include evaluation

--- tfm/crossmapping/vecmap/run_leftover.sh
+++ tfm/crossmapping/vecmap/run_leftover.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-
-for lang in fr de ru hi; do
-    for dimension in 400 500 1000; do
-        python -m tfm.crossmapping.vecmap.map_embeddings data/embeddings/monolingual-${lang}-3-6-${dimension}.vec data/embeddings/monolingual-en-3-6-${dimension}.vec data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec --batch_size 1000 --unsupervised --verbose
-    done
-done
-
 echo
 echo ------------ VECMAP ------------
 echo ------- \(BASE TOKENIZATION\) -------
@@ -38,36 +31,5 @@ for lang in fr de ru hi; do
         data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
         -d data/datasets/eval_dicts/${lang}-en.txt \
         --retrieval csls
-    done
-done
-
-echo ---\[L2 DISTANCE\]---
-echo LANG   ---   DIMENSION
-lang=de
-dimension=100
-echo ${lang}   ---   ${dimension}
-python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-    data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
-    data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-    -d data/datasets/eval_dicts/${lang}-en.txt \
-    --retrieval csls --dot
-for lang in ru hi; do
-    for dimension in 50 100; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-            data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
-            data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-            -d data/datasets/eval_dicts/${lang}-en.txt \
-            --retrieval csls --dot
-    done
-done
-for lang in fr de ru hi; do
-    for dimension in 400 500; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
-        data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls --dot
     done
 done
