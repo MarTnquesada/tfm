0fd9ff7e658d53144d3ee6f83ecc492d962a4cca
Martín Quesada <martin@datamaran.com>
2021-08-29 19:12:16 +0200

    updated run_leftover script

--- tfm/crossmapping/vecmap/run_leftover.sh
+++ tfm/crossmapping/vecmap/run_leftover.sh
@@ -5,26 +5,19 @@ echo ------------ VECMAP ------------
 echo ------- \(BASE TOKENIZATION\) -------
 echo ---\[COSINE SIMILARITY\]---
 echo LANG   ---   DIMENSION
-lang=de
-dimension=100
-echo ${lang}   ---   ${dimension}
-python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-    data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
-    data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-    -d data/datasets/eval_dicts/${lang}-en.txt \
-    --retrieval csls
-for lang in ru hi; do
-    for dimension in 50 100; do
+for lang in de ru hi; do
+    for dimension in 400 500; do
         echo ${lang}   ---   ${dimension}
         python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-            data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
-            data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
-            -d data/datasets/eval_dicts/${lang}-en.txt \
-            --retrieval csls
+        data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
+        data/embeddings/monolingual-en-3-6-${dimension}-vecmap-${lang}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls
     done
 done
+
 for lang in fr de ru hi; do
-    for dimension in 400 500; do
+    for dimension in 1000; do
         echo ${lang}   ---   ${dimension}
         python3 -m tfm.evaluation.evaluate_embedding_word_translation \
         data/embeddings/monolingual-${lang}-3-6-${dimension}-vecmap-en.vec \
