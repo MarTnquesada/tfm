dae81375147d814df56b342806eb6149f7c9474e
Martín Quesada <martin@datamaran.com>
2021-08-30 22:21:56 +0200

    modified run_evaluat_embedding_word_translation

--- tfm/evaluation/run_evaluate_embedding_word_translation.sh
+++ tfm/evaluation/run_evaluate_embedding_word_translation.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 echo
+echo ------------ CONCAT------------
+echo ------- \(BASE TOKENIZATION\) -------
+echo ---\[COSINE SIMILARITY\]---
+echo LANG   ---   DIMENSION
+for lang in ru hi; do
+    for dimension in 50; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls
+    done
+done
+
+echo
 echo ------------ MUSE ------------
 echo ------- \(BASE TOKENIZATION\) -------
 echo ---\[COSINE SIMILARITY\]---
@@ -11,25 +27,6 @@ for lang in fr de ru hi; do
         python3 -m tfm.evaluation.evaluate_embedding_word_translation \
         data/embeddings/${lang}-en-muse/${dimension}/vectors-${lang}.txt \
         data/embeddings/${lang}-en-muse/${dimension}/vectors-en.txt \
-        -d data/datasets/eval_dicts/${lang}-en.txt \
-        --retrieval csls
-    done
-done
-
-
-
-
-echo
-echo ------------ CONCAT------------
-echo ------- \(BASE TOKENIZATION\) -------
-echo ---\[COSINE SIMILARITY\]---
-echo LANG   ---   DIMENSION
-for lang in fr de ru hi; do
-    for dimension in 50 100 150 200 250 300; do
-        echo ${lang}   ---   ${dimension}
-        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
-        data/embeddings/monolingual-${lang}-en-3-6-${dimension}.vec \
         -d data/datasets/eval_dicts/${lang}-en.txt \
         --retrieval csls
     done
