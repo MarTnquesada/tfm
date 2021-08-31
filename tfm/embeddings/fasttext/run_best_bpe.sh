3e0791be2e5564f2f26f14a1e530aefec3296790
Martín Quesada <martin@datamaran.com>
2021-08-31 13:52:14 +0200

    corrected error in run_best_bpe in fasttext module

--- tfm/embeddings/fasttext/run_best_bpe.sh
+++ tfm/embeddings/fasttext/run_best_bpe.sh
@@ -4,6 +4,7 @@ for lang in fr de ru hi; do
     for dimension in 512; do
         ./fastText/fasttext skipgram -input ././../../../data/datasets/monolingual/monolingual.clean.${lang}.bpe_48000.${lang}-en -output ././../../../data/embeddings/monolingual-${lang}-3-6-${dimension}.bpe_48000.${lang}-en -minn 3 -maxn 6 -dim ${dimension} -epoch 5 -lr 0.05
         ./fastText/fasttext skipgram -input ././../../../data/datasets/monolingual/monolingual.clean.en.bpe_48000.${lang}-en -output ././../../../data/embeddings/monolingual-en-3-6-${dimension}.bpe_48000.${lang}-en -minn 3 -maxn 6 -dim ${dimension} -epoch 5 -lr 0.05
-        rm  ././../../../data/embeddings/monolingual-${lang}-3-6-${dimension}.bpe_48000.${lang}-en
+        rm  ././../../../data/embeddings/monolingual-${lang}-3-6-${dimension}.bpe_48000.${lang}-en.bin
+        rm  ././../../../data/embeddings/monolingual-en-3-6-${dimension}.bpe_48000.${lang}-en.bin
     done
 done
