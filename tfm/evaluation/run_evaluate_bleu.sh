4a8afd692536ffcdf092ca89c485c0632bc20064
Martín Quesada <martin@datamaran.com>
2021-08-31 16:25:17 +0200

    added run_evaluate_bleu script

--- tfm/evaluation/run_evaluate_bleu.sh
+++ tfm/evaluation/run_evaluate_bleu.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+for lang in fr de ru hi; do
+    onmt_translate -model data/models/trained/${lang}-en/run/model_step_40000.pt -src data/datasets/parallel/${lang}-en.test.clean.${lang} -output data/models/eval/pred_${lang}-en.txt -gpu 0 -verbose
+    docker container run -it --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/generic/multi-bleu.perl data/datasets/parallel/${lang}-en.test.clean.en < data/models/eval/pred_${lang}-en.txt"
+done
