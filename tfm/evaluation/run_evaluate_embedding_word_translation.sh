a0aba22ca85d529d0540d826a93e5ff70c3d62eb
Martín Quesada <martin@datamaran.com>
2021-09-03 15:31:09 +0200

    put MUSE as first method to evaluate

--- tfm/evaluation/run_evaluate_embedding_word_translation.sh
+++ tfm/evaluation/run_evaluate_embedding_word_translation.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 echo
+echo ------------ MUSE ------------
+echo ------- \(BASE TOKENIZATION\) -------
+echo ---\[COSINE SIMILARITY\]---
+echo LANG   ---   DIMENSION
+for lang in fr de ru hi; do
+    for dimension in 512; do
+        echo ${lang}   ---   ${dimension}
+        python3 -m tfm.evaluation.evaluate_embedding_word_translation \
+        data/embeddings/${lang}-en-muse/${dimension}/vectors-${lang}.txt \
+        data/embeddings/${lang}-en-muse/${dimension}/vectors-en.txt \
+        -d data/datasets/eval_dicts/${lang}-en.txt \
+        --retrieval csls
+    done
+done
+
+echo
 echo ------------ CONCAT------------
 echo ------- \(BASE TOKENIZATION\) -------
 echo ---\[COSINE SIMILARITY\]---
