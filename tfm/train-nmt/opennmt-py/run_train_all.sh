0b861ec5ca571a384680dd72b1fd125b02257d41
Martín Quesada <martin@datamaran.com>
2021-09-04 23:07:45 +0200

    added launching de opennmt training

--- tfm/train-nmt/opennmt-py/run_train_all.sh
+++ tfm/train-nmt/opennmt-py/run_train_all.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Non-BPE
+
+# BPE
+for lang in en fr de ru hi; do
+    for method in base muse vecmap concat; do
+        for dimension in 512; do
+            for weights in frozen unfrozen; do
+                mkdir data/models/trained/${lang-en}_${dimension}_${method}_${weights}
+                ./tfm/train-nmt/opennmt-py/train.sh -c ./tfm/train-nmt/opennmt-py/configs/${lang-en}/${lang-en}_${dimension}_${method}_${weights}.yml
+            done
+        done
+    done
+done
+
 # BPE
 for lang in en fr de ru hi; do
     for method in base muse vecmap concat; do
---
+++ tfm/train-nmt/opennmt-py/run_train_de_unfrozen_bpe.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for lang in de; do
+    for method in base muse vecmap concat; do
+        for dimension in 512; do
+            for weights in unfrozen; do
+                mkdir data/models/trained/${lang-en}_${dimension}_${method}_${weights}_bpe/
+                ./tfm/train-nmt/opennmt-py/train.sh -c ./tfm/train-nmt/opennmt-py/configs/${lang-en}/${lang-en}_${dimension}_${method}_${weights}_bpe.yml -b true
+            done
+        done
+    done
+done
