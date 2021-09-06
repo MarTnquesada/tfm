d9fc94c1efb4b5d99b7ab25b6839451019b78c95
Martín Quesada <martin@datamaran.com>
2021-09-06 23:36:06 +0200

    fixed concat onmt training being incorrectly frozen in concat_unfrozen

--- tfm/train-nmt/opennmt-py/run_train_fr_ru_unfrozen_bpe_plus_all_frozen_bpe.sh
+++ tfm/train-nmt/opennmt-py/run_train_fr_ru_unfrozen_bpe_plus_all_frozen_bpe.sh
@@ -11,3 +11,15 @@ for lang in fr ru; do
         done
     done
 done
+
+# BPE
+for lang in fr ru de hi; do
+    for method in base muse vecmap concat; do
+        for dimension in 512; do
+            for weights in frozen; do
+                mkdir data/models/trained/${lang}-en_${dimension}_${method}_${weights}_bpe/
+                ./tfm/train-nmt/opennmt-py/train.sh -c ./tfm/train-nmt/opennmt-py/configs/${lang}-en/${lang}-en_${dimension}_${method}_${weights}_bpe.yml -b true
+            done
+        done
+    done
+done
--- tfm/train-nmt/opennmt-py/run_train_hi_unfrozen_bpe.sh
+++ tfm/train-nmt/opennmt-py/run_train_hi_unfrozen_bpe.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 # BPE
+
+# Fix the DE error
+
+for lang in de; do
+    for method in concat; do
+        for dimension in 512; do
+            for weights in unfrozen; do
+                mkdir data/models/trained/${lang}-en_${dimension}_${method}_${weights}_bpe/
+                ./tfm/train-nmt/opennmt-py/train.sh -c ./tfm/train-nmt/opennmt-py/configs/${lang}-en/${lang}-en_${dimension}_${method}_${weights}_bpe.yml -b true
+            done
+        done
+    done
+done
+
 for lang in hi; do
     for method in base muse vecmap concat; do
         for dimension in 512; do
