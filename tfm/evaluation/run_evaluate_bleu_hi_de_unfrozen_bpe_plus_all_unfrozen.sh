51357fd570c3e284edc2d6265bec41e04049f7a7
Martín Quesada <martnquesada@gmail.com>
2021-09-11 04:16:08 +0200

    added bleu evaluation for existing models

--- tfm/evaluation/run_evaluate_bleu_hi_de_unfrozen_bpe_plus_all_unfrozen.sh
+++ tfm/evaluation/run_evaluate_bleu_hi_de_unfrozen_bpe_plus_all_unfrozen.sh
@@ -5,7 +5,7 @@ for lang in de hi; do
   for method in base muse vecmap concat; do
     echo "${lang} -- ${method} -- unfrozen - bpe"
     onmt_translate -model data/models/trained/${lang}-en_512_${method}_unfrozen_bpe/model_step_20000.pt -src data/datasets/parallel/${lang}-en.test.clean.bpe_48000.${lang} -output data/models/eval/pred_${lang}-en_512_${method}_unfrozen_bpe.txt -gpu 0
-    docker container run --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/generic/multi-bleu.perl /data/datasets/parallel/${lang}-en.test.clean.bpe_48000.en < /data/models/eval/pred_${lang}-en-512.txt"
+    docker container run --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/generic/multi-bleu.perl /data/datasets/parallel/${lang}-en.test.clean.bpe_48000.en < /data/models/eval/pred_${lang}-en_512_${method}_unfrozen_bpe.txt"
   done
 done
 
@@ -13,7 +13,7 @@ done
 for lang in fr de ru hi; do
   for method in base muse vecmap concat; do
     echo "${lang} -- ${method} -- unfrozen - nonbpe"
-    onmt_translate -model data/models/trained/${lang}-en_512_${method}_unfrozen/model_step_20000.pt -src data/datasets/parallel/${lang}-en.test.clean.${lang} -output data/models/eval/pred_${lang}-en_512_${method}_unfrozen_bpe.txt -gpu 0
-    docker container run --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/generic/multi-bleu.perl /data/datasets/parallel/${lang}-en.test.clean.en < /data/models/eval/pred_${lang}-en_512_${method}_unfrozen_bpe.txt"
+    onmt_translate -model data/models/trained/${lang}-en_512_${method}_unfrozen/model_step_20000.pt -src data/datasets/parallel/${lang}-en.test.clean.${lang} -output data/models/eval/pred_${lang}-en_512_${method}_unfrozen.txt -gpu 0
+    docker container run --rm -v ${PWD}/data/:/data moses /bin/bash -c "scripts/generic/multi-bleu.perl /data/datasets/parallel/${lang}-en.test.clean.en < /data/models/eval/pred_${lang}-en_512_${method}_unfrozen.txt"
   done
 done
