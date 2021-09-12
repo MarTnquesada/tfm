8b5ea893e1b6fea298a9ca2b8c75f45637a71a2b
Martín Quesada <martnquesada@gmail.com>
2021-09-12 14:58:04 +0200

    updated embedding plotting

--- visualizations/plot_embedding.py
+++ visualizations/plot_embedding.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument('--embedding', default="../data/embeddings/de-en-muse/512-bpe/vectors-de.txt")
     parser.add_argument('--color', default="blue")
     parser.add_argument('--top_k', default=20000, type=int)
-    parser.add_argument('--words', default=[])
+    parser.add_argument('--words', default=['animal','newspaper','government','science','residents','police','economy'])
     parser.add_argument('--path', default='muse-de-en-bpe.de.pdf')
     args = parser.parse_args()
 
@@ -38,7 +38,10 @@ def main():
 
     tsne_ak_2d = TSNE(perplexity=30, n_components=2, init='pca', n_iter=3500, random_state=32)
     #emb.sort_by_descending_frequency()  -- the KeyedVectors object is already sorted by default
-    vectors = tsne_ak_2d.fit_transform([emb[n] for n in range(0, args.top_k)])
+    if args.words:
+        vectors = tsne_ak_2d.fit_transform([emb[w] for w in args.words])
+    else:
+        vectors = tsne_ak_2d.fit_transform([emb[n] for n in range(0, args.top_k)])
 
     tsne_plot_2d(vectors, a=0.1, color=args.color, words=args.words, path=args.path)
 
